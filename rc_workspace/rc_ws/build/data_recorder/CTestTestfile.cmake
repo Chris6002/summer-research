d23a63894aaf66e8c9412b303e63ade1b3b98ba7
bflippedyx <jbi5@ur.rochester.edu>
2018-07-27 15:21:08 -0400

    remove ROS support

--- rc_workspace/rc_ws/build/data_recorder/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vision/summer-research/rc_workspace/rc_ws/src/data_recorder
-# Build directory: /home/vision/summer-research/rc_workspace/rc_ws/build/data_recorder
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

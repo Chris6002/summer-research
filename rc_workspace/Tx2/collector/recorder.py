a401479e0678b24e1868d2d0915c56e13f7f85aa
JIng Bi <jbi5@ur.rochester.edu>
2018-08-03 10:27:58 -0400

    finish recorder

--- rc_workspace/Tx2/collector/recorder.py
+++ rc_workspace/Tx2/collector/recorder.py
@@ -1,87 +1,72 @@
-
 import numpy as np
 import time
 import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import cv2
-'''
-##################################################
-#################  CAMERA SETUP  #################
-##################################################
-'''
+'''#################  GLOBAL PARAMETER  #################'''
+resolution = [640, 480]
+record_FPS = 10
+'''#################  CAMERA SETUP  #################'''
 '''##############  Realsense  ##############'''
 # Configure depth and color streams
 import pyrealsense2 as rs
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.color, resolution[0], resolution[1],
+                     rs.format.bgr8, 30)
 # Start streaming
 pipeline.start(config)
 '''##############  Logitech  ##############'''
-
-'''
-##################################################
-################  SERVO & SERIES  ################
-##################################################
-'''
+cap1 = cv2.VideoCapture(1)
+cap1.set(3, resolution[0])
+cap1.set(4, resolution[1])
+cap2 = cv2.VideoCapture(2)
+cap2.set(3, resolution[0])
+cap2.set(4, resolution[1])
+'''################  SERVO & SERIES  ################'''
 '''##############  Polotu  ##############'''
 import maestro
 servo = maestro.Controller()
+import maestro
+servo = maestro.Controller('/dev/ttyACM0')
+servo.setRange(0, 1000 * 4, 2000 * 4)
+servo.setSpeed(0, 35)
+servo.setRange(1, 1000 * 4, 2500 * 4)
 '''##############  Arduino  ##############'''
 import serial
-ser=serial.Serial('/dev/ttyTHS2')  # open serial port
+ser = serial.Serial(port='/dev/ttyTHS2', baudrate=115200)  # open serial port
+ser.flushInput()
+ser.flushOutput()
 
-
-
-servo.setAccel(0,4)      #set servo 0 acceleration to 4
-servo.setTarget(0,6000)  #set servo to move to center position
-servo.setSpeed(1,10)     #set speed of servo 1
-x = servo.getPosition(1) #get the current position of servo 1
-
-
-record_FPS=10
 try:
-    starttime=time.time()
+    starttime = time.time()
     while True:
-
-        '''##############  Get image  ##############'''
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
         '''##############  Get command  ##############'''
-        command = ser.readline()
-        '''!TODO:
-        ch1=command[xxx]
-        ch2=command[xxx]
-        ch3=command[xxx]
-        ch4=command[xxx]
-        '''
+        command = ser.readline().decode('utf-8').rstrip().split('x')
+        ch1 = int(command[0])
+        ch2 = int(command[1])
+        ch3 = int(command[2])
+        '''##############  Get image  ##############'''
+        ret, frame1 = cap1.read()
+        ret, frame2 = cap2.read()
+        frames3 = pipeline.wait_for_frames()
+        color_frame3 = frames3.get_color_frame()
+        color_image = np.asanyarray(color_frame3.get_data())
         ###!!!!!!!!!Read a line which is terminated with end-of-line (eol) character (\n by default) or until timeout.
-        
         '''##############  SAVE  ##############'''
-
-
+        #！TODO： 添加folder
+        cv2.imwrite('/media/nvidia/Files/1_' + str(i) + '.png', frame1)
+        cv2.imwrite('/media/nvidia/Files/1_' + str(i) + '.png', frame1)
+        cv2.imwrite('/media/nvidia/Files/1_' + str(i) + '.png', frame1)
         # Convert images to numpy arrays
-
-        color_image = np.asanyarray(color_frame.get_data())
-
-
-        # Stack both images horizontally
-        images =color_image
-        #print('hehe')
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
-        
-
-
         '''##############  Send command  ##############'''
-        servo.setTarget(0,ch1)  #set servo to move to center position
-        servo.setTarget(1,ch2)  #set servo to move to center position
-        servo.setTarget(2,ch3)  #set servo to move to center position
-        servo.setTarget(3,ch4)  #set servo to move to center position
+        servo.setTarget(0, ch1)  #set servo to move to center position
+        servo.setTarget(1, ch2)  #set servo to move to center position
+        servo.setTarget(2, ch3)  #set servo to move to center position
+        servo.setTarget(3, ch4)  #set servo to move to center position
         '''##############  Keep Frequence  ##############'''
-        time.sleep(1/record_FPS-((time.time() - starttime) % (1/record_FPS)))
+        time.sleep(1 / record_FPS - (
+            (time.time() - starttime) % (1 / record_FPS)))
 finally:
 
     # Stop streaming

2185d1b734b282108623d3bfb2253db34c111d03
JIng Bi <jbi5@ur.rochester.edu>
2018-08-07 12:23:35 -0400

    finish Master&Slave

--- rc_workspace/Tx2/collector/Master.py
+++ rc_workspace/Tx2/collector/Master.py
@@ -1,16 +1,130 @@
 import time
+import sys
+import os
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+import cv2
+import numpy as np
+# =====================================
+# Network Configuration
+# =====================================
+
 from multiprocessing.connection import Client
-
-record_FPS = 10
 c = Client(('localhost', 25000), authkey=b'peekaboo')
-try:
-    starttime = time.time()
+# =====================================
+# Global setting
+# =====================================
+camera_left = '/media/nvidia/Files/Left/'
+camera_right = '/media/nvidia/Files/Right/'
+camera_center = '/media/nvidia/Files/Center/'
+resolution = [640, 480]
+record_FPS = 10
+speed_range = [1400, 1750]
+steer_range = [1000, 1800]
 
+
+# =====================================
+# Convenient function
+# =====================================
+def createFolder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+
+def limitValue(n, minn, maxn):
+    return max(min(maxn, n), minn)
+
+
+# =====================================
+# Camera setup
+# =====================================
+import pyrealsense2 as rs
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, resolution[0], resolution[1],
+                     rs.format.bgr8, 30)
+#config.enable_stream(rs.stream.depth, resolution[0], resolution[1],
+#                     rs.format.z16, 30)
+# Start streaming
+pipeline.start(config)
+# =====================================
+# SERVO & SERIES
+# =====================================
+#  Polotu
+import maestro
+servo = maestro.Controller()
+import maestro
+servo = maestro.Controller('/dev/ttyACM0')
+servo.setRange(0, 1000 * 4, 2000 * 4)
+servo.setSpeed(0, 45)
+servo.setRange(1, 1000 * 4, 2500 * 4)
+#  Arduino
+import serial
+ser = serial.Serial(port='/dev/ttyTHS2', baudrate=115200)  # open serial port
+ser.flushInput()
+ser.flushOutput()
+# =====================================
+# CSV
+# =====================================
+
+import csv
+createFolder('/media/nvidia/Files/Left')
+createFolder('/media/nvidia/Files/Right')
+createFolder('/media/nvidia/Files/Center')
+f = open('/media/nvidia/Files/data_c.csv', 'w')
+fnames = ['name', 'steering', 'speed', 'category']
+writer = csv.DictWriter(f_c, fieldnames=fnames)
+writer.writeheader()
+
+# =====================================
+# Main Function
+# =====================================
+try:
+    iter_num = 0
+    ch3_pre = 0
     while True:
         starttime = time.time()
-        c.send('hello')
+        # ======  Get command  ====== #
+        ser.flushInput()
+        command = ser.readline().decode('utf-8').rstrip().split('x')
+        ch1, ch2, ch3 = int(command[0]), int(command[1]), int(command[2])
+        # =========================== #
+        if ch3 - ch3_pre > 100:
+
+            iter_num = iter_num + 1
+            print(iter_num)
+            c.send('Iter:' + str(iter_num))
+            out = cv2.VideoWriter(
+                '/media/nvidia/Files/Center/' + str(iter_num) + '.avi',
+                cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10,
+                (resolution[0], resolution[1]))
+        if ch3_pre > 1500:
+            # ======  Get Image  ====== #
+            real_frames = pipeline.wait_for_frames()
+            color_frame = real_frames.get_color_frame()
+            color_image = np.asanyarray(color_frame.get_data())
+            # ========================= #
+            data = {
+                'name': iter_num,
+                'steering': ch1,
+                'speed': ch2,
+                'category': 0
+            }
+            # ======  Save  ====== #
+            c.send('Save')
+            out.write(color_image)
+            writer.writerow(data)
+            # ==================== #
+        # ======  Send command  ====== #
+        ch1 = limitValue(ch1, steer_range[0], steer_range[1])
+        ch2 = limitValue(ch2, speed_range[0], speed_range[1])
+        servo.setTarget(0, ch1 * 4)  #set servo to move to center position
+        servo.setTarget(1, ch2 * 4)  #set servo to move to center position
+        # ============================ #
         time.sleep(1 / record_FPS - (
             (time.time() - starttime) % (1 / record_FPS)))
+        ch3_pre = ch3
+
         print(time.time() - starttime)
+
 finally:
     print('finished')
--- rc_workspace/Tx2/collector/Slave.py
+++ rc_workspace/Tx2/collector/Slave.py
@@ -1,12 +1,88 @@
 import time
+
+# =====================================
+# Network Configuration
+# =====================================
 from multiprocessing.connection import Listener
 serv = Listener(('', 25000), authkey=b'peekaboo')
 
+
+# =====================================
+# Camera setup
+# =====================================
+class WebcamVideoStream:
+    def __init__(self, src=0, width=1280, height=720):
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(3, width)
+        self.stream.set(4, height)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.started = False
+        self.read_lock = Lock()
+
+    def start(self):
+        if self.started:
+            print("already started!!")
+            return None
+        self.started = True
+        self.thread = Thread(target=self.update, args=())
+        self.thread.start()
+        return self
+
+    def update(self):
+        while self.started:
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
+
+    def read(self):
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stream.release()
+
+
+vs1 = WebcamVideoStream(
+    '/dev/v4l/by-id/usb-046d_HD_Webcam_C615_794F2390-video-index0').start()
+vs2 = WebcamVideoStream(
+    '/dev/v4l/by-id/usb-046d_HD_Webcam_C615_06D65490-video-index0').start()
+out1 = cv2.VideoWriter('/media/nvidia/Files/Left/' + '0.avi',
+                       cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10,
+                       (1280, 720))
+out2 = cv2.VideoWriter('/media/nvidia/Files/Right/' + '0.avi',
+                       cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10,
+                       (1280, 720))
+iter_num = 0
 try:
     client = serv.accept()
     while True:
         starttime = time.time()
-        msg = client.recv()
+        msg = client.recv().split(':')
+        if msg[0] == 'Iter':
+            print('creating...')
+            if iter_num > 0:
+                out1.release()
+                out2.release()
+            iter_num = msg[1]
+            out1 = cv2.VideoWriter(
+                '/media/nvidia/Files/Left/' + str(iter_num) + '.avi',
+                cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10, (1280, 720))
+            out2 = cv2.VideoWriter(
+                '/media/nvidia/Files/Right/' + str(iter_num) + '.avi',
+                cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'), 10, (1280, 720))
+        elif msg[0] == 'Save':
+            out1.write(vs1.read())
+            out2.write(vs2.read())
         print(time.time() - starttime)
 finally:
-    print('finished')
+    out1.release()
+    out2.release()
+    vs1.stop()
+    vs2.stop()

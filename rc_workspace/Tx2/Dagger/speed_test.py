c9ed34200d96acea6d5dda6b9c04ee22f58375fc
JIng Bi <jbi5@ur.rochester.edu>
2018-09-03 11:29:48 -0400

    start training

---
+++ rc_workspace/Tx2/Dagger/speed_test.py
@@ -0,0 +1,101 @@
+import numpy as np
+import sys
+import time
+from director import Monitor
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+import cv2
+
+# =====================================
+# Loading network
+# =====================================
+
+monitor=Monitor('./checkpoint_07.pth.tar')
+
+
+# =====================================
+# Global setting
+# =====================================
+def createFolder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+folder_path= '/media/nvidia/Files/speed_test'
+createFolder(folder_path)
+name='I AM CENTRE'
+print(name)
+resolution = (640, 480)
+record_FPS = 10
+frequence = 1 / record_FPS
+FourCC = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+device = '/dev/v4l/by-id/usb-Intel_R__RealSense_TM__415_Intel_R__RealSense_TM__415_811313022233-video-index0'
+# port_num = 25002
+# =====================================
+# Network Configuration
+# =====================================
+# from multiprocessing.connection import Client
+# client = Client(('localhost', port_num), authkey=b'peekaboo')
+
+# =====================================
+# Camera setup
+# =====================================
+cap = cv2.VideoCapture(device)
+cap.set(3, resolution[0])
+cap.set(4, resolution[1])
+bool_retrieve = cap.grab()
+ret, frame = cap.retrieve()
+# =====================================
+# Init value
+# =====================================
+start_time = time.time()
+end_time = time.time()
+iter_num = 0
+out = cv2.VideoWriter(folder_path + '0.avi', FourCC, record_FPS,
+                      resolution)
+index=0
+try:
+    while True:
+        index=index+1
+        start_time = time.time()
+        msg = client.recv().split(':')
+        bool_retrieve = cap.grab()
+        previous_frame=frame
+        ret, frame = cap.retrieve()
+        if bool_retrieve:
+            if msg[0] == 'Iter':
+                print('creating...')
+                if iter_num > 0:
+                    out.release()
+                iter_num = int(msg[1])
+                out = cv2.VideoWriter(folder_path + msg[1]+'.avi', FourCC, record_FPS,
+                                    resolution)
+            elif msg[0] == 'Save':
+                # ===============================
+                output=monitor.inference(frame)
+                print(output)
+                # ===============================
+                out.write(frame)
+                if index>20:
+                    index=0
+                    print('Saving', end='   ')
+                    print(iter_num, end='   ')
+                    print(str(round(1/(time.time()-start_time),1)) )
+            elif msg[0] == 'Waiting':
+                if index>20:
+                    index=0
+                    print('Waiting', end='   ')
+                    print(str(round(1/(time.time()-start_time),1)) )
+        else:
+            out.write(previous_frame)
+            out.write(previous_frame)
+            out.write(previous_frame)
+            out.write(previous_frame)
+            cap.release()
+            cap = cv2.VideoCapture(device)
+            cap.set(3, resolution[0])
+            cap.set(4, resolution[1])
+        print(time.time()-start_time)
+        #if (now - start_time) < frequence:time.sleep(frequence - ((now - start_time) % frequence))
+        
+
+finally:
+    out.release()
+    cap.release()

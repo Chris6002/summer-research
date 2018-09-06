da15059519867d53df5cf5738a5bb2846f54feb5
JIng Bi <jbi5@ur.rochester.edu>
2018-09-06 08:39:33 -0400

    finish all code!!!!!!!

--- g7_workspace/data/dataagg_flip.py
+++ g7_workspace/data/dataagg_flip.py
@@ -1,9 +1,15 @@
 import csv
 import os
 from PIL import Image
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--oldpath',  type=str)
+parser.add_argument('--newpath', type=str)
+args = parser.parse_args()
 dir_path = os.path.dirname(os.path.realpath(__file__))
-old_path=os.path.join(dir_path,'dataset/command/0/all_three.csv')
-new_path=os.path.join('dataset/command/0/test.csv')
+old_path=os.path.join(dir_path,args.oldpath)
+new_path=os.path.join(dir_path,args.newpath)
 print(dir_path)
 
 with open(old_path) as in_file, open(new_path,'w') as out_file:

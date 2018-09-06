70c5eb4d8c5e0d3e6dc511f0de570fd9c2b07bf9
JIng Bi <jbi5@ur.rochester.edu>
2018-09-06 08:35:50 -0400

    finish all code!!!!!!!

--- g7_workspace/data/flip_onlyleft.py
+++ g7_workspace/data/flip_onlyleft.py
@@ -3,7 +3,10 @@ import os
 from PIL import Image
 dir_path = os.path.dirname(os.path.realpath(__file__))
 old_path=os.path.join(dir_path,'dataset/command/0/all_three.csv')
+
 new_path=os.path.join('dataset/command/0/test_left.csv')
+# old_path=os.path.join(dir_path,'0_command.csv')
+# new_path=os.path.join('test_left.csv')
 print(dir_path)
 
 with open(old_path) as in_file, open(new_path,'w') as out_file:
@@ -13,7 +16,7 @@ with open(old_path) as in_file, open(new_path,'w') as out_file:
     csv_writer.writeheader()
 
     for index,row in enumerate(csv_reader):
-        if 'right' not in row:
+        if 'right' not in row['name']:
             csv_writer.writerow(row)
             new_row=row.copy()
             new_row['name']=row['name']+'filp'
